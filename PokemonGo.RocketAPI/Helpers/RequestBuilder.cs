a1410798e7c3943f532987ac93c2dfd013886d5f
Aquilum <tom@fleetcom.org>
2016-08-11 23:57:26 +0100

    RequestBuilder: Use managed encrypt
    Rather than using a precompiled C library for the PoGo encryption, let's
    use the entirely managed code encryption library.
    
    This should buy us platform-agnosticism, as the cost of doubling a
    single packet's encode time (which should still be tiny on any 'real'
    platform).

--- PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
+++ PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
@@ -121,32 +121,10 @@ namespace PokemonGo.RocketAPI.Helpers
         }
         private byte[] Encrypt(byte[] bytes)
         {
-            var outputLength = 32 + bytes.Length + (256 - (bytes.Length % 256));
-            var ptr = Marshal.AllocHGlobal(outputLength);
-            var ptrOutput = Marshal.AllocHGlobal(outputLength);
-            FillMemory(ptr, (uint)outputLength, 0);
-            FillMemory(ptrOutput, (uint)outputLength, 0);
-            Marshal.Copy(bytes, 0, ptr, bytes.Length);
-            try
-            {
-                int outputSize = outputLength;
-                EncryptNative(ptr, bytes.Length, new byte[32], 32, ptrOutput, out outputSize);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            var output = new byte[outputLength];
-            Marshal.Copy(ptrOutput, output, 0, outputLength);
-            Marshal.FreeHGlobal(ptr);
-            Marshal.FreeHGlobal(ptrOutput);
-            return output;
+            byte[] iv = new byte[32];
+            new Random().NextBytes(iv);
+            PokemonGoEncryptSharp.Util.Encrypt(bytes, iv);
         }
-
-        [DllImport("Resources/encrypt.dll", EntryPoint = "encrypt", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-        static extern private void EncryptNative(IntPtr arr, int length, byte[] iv, int ivsize, IntPtr output, out int outputSize);
-        [DllImport("kernel32.dll", EntryPoint = "RtlFillMemory", SetLastError = false)]
-        static extern void FillMemory(IntPtr destination, uint length, byte fill);
 
         public RequestEnvelope GetRequestEnvelope(params Request[] customRequests)
         {
