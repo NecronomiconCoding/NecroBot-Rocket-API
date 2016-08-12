ac18393de0b3833626ebfaed628bbc9e57eb87d0
Kevin Collmer <kevin.collmer@googlemail.com>
2016-08-12 13:21:03 +0200

    Reworked Encryption

--- PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
+++ PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
@@ -116,37 +116,11 @@ namespace PokemonGo.RocketAPI.Helpers
             Unknown6 val = new Unknown6();
             val.RequestType = 6;
             val.Unknown2 = new Unknown6.Types.Unknown2();
-            val.Unknown2.Unknown1 = ByteString.CopyFrom(Encrypt(sig.ToByteArray()));
+            var iv = new byte[32];
+            new Random().NextBytes(iv);
+            val.Unknown2.Unknown1 = ByteString.CopyFrom(NecroBot_Network_Logic.Encryption.Encrypt(sig.ToByteArray(), iv));
             return val;
         }
-        private byte[] Encrypt(byte[] bytes)
-        {
-            var outputLength = 32 + bytes.Length + (256 - (bytes.Length % 256));
-            var ptr = Marshal.AllocHGlobal(outputLength);
-            var ptrOutput = Marshal.AllocHGlobal(outputLength);
-            FillMemory(ptr, (uint)outputLength, 0);
-            FillMemory(ptrOutput, (uint)outputLength, 0);
-            Marshal.Copy(bytes, 0, ptr, bytes.Length);
-            try
-            {
-                int outputSize = outputLength;
-                EncryptNative(ptr, bytes.Length, new byte[32], 32, ptrOutput, out outputSize);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            var output = new byte[outputLength];
-            Marshal.Copy(ptrOutput, output, 0, outputLength);
-            Marshal.FreeHGlobal(ptr);
-            Marshal.FreeHGlobal(ptrOutput);
-            return output;
-        }
-
-        [DllImport("Resources/encrypt.dll", EntryPoint = "encrypt", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
-        static extern private void EncryptNative(IntPtr arr, int length, byte[] iv, int ivsize, IntPtr output, out int outputSize);
-        [DllImport("kernel32.dll", EntryPoint = "RtlFillMemory", SetLastError = false)]
-        static extern void FillMemory(IntPtr destination, uint length, byte fill);
 
         public RequestEnvelope GetRequestEnvelope(params Request[] customRequests)
         {
